104029fa9dce0e5de21af965704eef574e1beb5d
FSWL <fswl98@gmail.com>
2022-02-09 00:03:00 +0300

    trying to build

--- src/components/FavoritesPage/FavoritesPage.tsx
+++ src/components/FavoritesPage/FavoritesPage.tsx
@@ -3,12 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
 
 import './FavoritesPage.scss';
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 const FavoritesPage = () => {
     return (
         <div className="FavoritesPage">
             <span>Сервис находится в разработке</span>
-            <FontAwesomeIcon icon={faCog} spin size='3x' />
+            <FontAwesomeIcon icon={ faCog as IconProp } spin size='3x' />
         </div>
     );
 };
